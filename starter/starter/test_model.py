c5cf962ef0620c77edf8d3a6809ec179ad1e26d7
Jakrapun Sangchan <jakrapun.san@gmail.com>
2022-11-17 23:23:39 +1100

    test8

--- starter/starter/test_model.py
+++ starter/starter/test_model.py
@@ -10,7 +10,6 @@ import os
 
 def test_model():
     path = 'starter/model/ML_model.pkl'
-    path.mkdir(parents=True, exist_ok=True)
     model = pickle.load(open(path,'rb'))
 
     assert model.get_params()['max_iter'] == 300
