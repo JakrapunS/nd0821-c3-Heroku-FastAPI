7bb491f957e25d20b77c012bb41d2a45621c7ed6
Jakrapun Sangchan <jakrapun.san@gmail.com>
2022-11-17 22:07:32 +1100

    test 3

--- starter/Test/test_model.py
+++ starter/Test/test_model.py
@@ -1,18 +1,18 @@
 """
 Unit test for ml model
 """
-import pickle
+from starter.ml.model import train_model
+from starter.train_model import model
 import os
 
-def test_n_class():
-    model = pickle.load(open(os.path.join('./', "nd0821-c3-Heroku-FastAPI/starter/model/ML_model.pkl"), 'rb'))
+def test_n_class(model):
+    
     assert model.n_classes_ == 2
 
-def test_min_sample_leaf():
-    model = pickle.load(open(os.path.join('./', "nd0821-c3-Heroku-FastAPI/starter/model/ML_model.pkl"), 'rb'))
+def test_min_sample_leaf(model):
+    
     assert model.get_params()['min_samples_leaf'] == 5
 
-def test_min_samples_split():
-    model = pickle.load(open( os.path.join('./', "nd0821-c3-Heroku-FastAPI/starter/model/ML_model.pkl"),'rb'))
+def test_min_samples_split(model):
 
     assert model.get_params()['min_samples_split'] == 10
