3374ca2cc18775af713fdb3cdf0f735cb3943466
Jakrapun Sangchan <jakrapun.san@gmail.com>
2022-11-17 22:54:59 +1100

    test 7

--- starter/starter/test_model.py
+++ starter/starter/test_model.py
@@ -5,9 +5,7 @@ Unit test for ml model
 import pickle
 import pandas as pd
 
-model = pickle.load(open('/home/jakrapun/Heroku/nd0821-c3-Heroku-FastAPI/starter/model/ML_model.pkl','rb'))
 
-print(model.get_params())
 def test_model():
     model = pickle.load(open('/starter/model/ML_model.pkl','rb'))
     assert model.get_params()['max_iter'] == 300
