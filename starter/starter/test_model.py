635a1ddeba7e23c2a36cdbc83dd2e122a063053e
Jakrapun Sangchan <jakrapun.san@gmail.com>
2022-11-17 23:34:59 +1100

    test_10

--- starter/starter/test_model.py
+++ starter/starter/test_model.py
@@ -5,14 +5,22 @@ Unit test for ml model
 import pickle
 import pandas as pd
 import os
-import sklearn
 
 
-
-def test_model():
-    path = 'starter/model/ML_model.pkl'
+def test_model_min_leaf():
+    path = 'starter/model/RandomForest.pkl'
     model = pickle.load(open(path,'rb'))
 
-    assert model.get_params()['max_iter'] == 300
+    assert model.get_params()['min_samples_leaf'] == 5
     
+def test_model_min_split():
+    path = 'starter/model/RandomForest.pkl'
+    model = pickle.load(open(path,'rb'))
 
+    assert model.get_params()['min_samples_split'] == 10
+
+def test_model_n_estimators():
+    path = 'starter/model/RandomForest.pkl'
+    model = pickle.load(open(path,'rb'))
+
+    assert model.get_params()['n_estimators'] == 100
