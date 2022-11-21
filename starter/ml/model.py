f0faed8d6d61bce4a9999d9f0f275bd806ac5c0c
Jakrapun Sangchan <jakrapun.san@gmail.com>
2022-11-21 21:45:43 +1100

    pickle

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-import pickle
+import pickle5 as pickle
 import pandas as pd
 import numpy as np
 
